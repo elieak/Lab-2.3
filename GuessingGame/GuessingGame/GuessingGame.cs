7d3d5f89d28d3cbd4e908ea1acf7b2ff34c7102c
elie <eliea@code-college.net>
2016-06-20 01:13:42 +0300

    working

--- GuessingGame/GuessingGame/GuessingGame.cs
+++ GuessingGame/GuessingGame/GuessingGame.cs
@@ -1,46 +1,42 @@
 ﻿using System;
+using static System.Console;
 
 namespace GuessingGame
 {
-    public static class CheckUserInput
+    internal static class GuessingGame
     {
-        public static string InputCheck(int usrInput, int secretNumber, int count, int maxGuesses)
+        private static void Main()
         {
-            if (usrInput == secretNumber && count <= maxGuesses)
+            WriteLine("Welcome to Guess a Word Program (0-100) with in 7 attempts.");
+            var rand = new Random();
+            var secretNumber = rand.Next(1, 101);
+            const int maxTries = 7;
+
+            var isGuessed = false;
+
+            for (var i = 1; i <= maxTries; i++)
             {
-                return ($"Congrats it took you " + count + "of " + maxGuesses + " tries to guess the secret number " +
-                        secretNumber);
+                Write($"ATTEMPT {i} : Enter your number: ");
+                int userInput;
+                int.TryParse(ReadLine(), out userInput);
+                if (userInput == secretNumber)
+                {
+                    WriteLine($"Congrats! You have guessed the number correctly in {i} tries");
+                    isGuessed = true;
+                    break;
+                }
+                if (userInput > secretNumber)
+                {
+                    WriteLine("Number too big");
+                }
+                if (userInput < secretNumber)
+                {
+                    WriteLine("Number too small");
+                }
             }
-            if (usrInput > secretNumber && count <= maxGuesses)
+            if (isGuessed == false)
             {
-                return ($"Number is too big, try again: ");
-            }
-            if (usrInput < secretNumber && count <= maxGuesses)
-            {
-                return ($"Number is too small, try again: ");
-            }
-            return ($"You Failed! The number was: " + secretNumber);
-        }
-    }
-
-    class GuessingGame
-    {
-        static void Main()
-        {
-            Random rand = new Random();
-            int secret = rand.Next(1, 101);
-            const int MaxGuesses = 7;
-            int userInput;
-            bool isGuessed = false;
-
-            Console.WriteLine("--- The computer has generated a secret number between 1 - 100, Try to guess it ---");
-
-            Console.Write("Enter your guess: ");
-            int.TryParse(Console.ReadLine(), out userInput);
-
-            for (int count = 0; count <= MaxGuesses; ++count)
-            {
-                Console.Write(count + ": " + secret + " : " + CheckUserInput.InputCheck(userInput, secret, count, MaxGuesses));
+                WriteLine("Unfortunately you did not guess it correctly. The correct number is: " + secretNumber);
             }
         }
     }
